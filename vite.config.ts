fd572c1d68a853e1d5b83842b1cae1e8aea76084
Neytrino2134 <neytrino2134@gmail.com>
2026-01-08 06:26:06 +0500

    upd vite cfg

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, (process as any).cwd(), '');
 
+  // Опрелеляем, является ли это сборкой специально для GitHub Pages
+  const isGhPages = mode === 'gh-pages';
+
   return {
     plugins: [react()],
-    // Базовый путь должен совпадать с именем вашего репозитория на GitHub
-    base: '/Script-Modifier/',
+    // Если режим gh-pages, используем имя репозитория. В противном случае (Netlify/Local) используем корень.
+    base: isGhPages ? '/Script-Modifier/' : '/',
     define: {
       // Безопасная замена глобальных переменных
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
